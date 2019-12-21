5af810263b6b81291672d83b07c7a295143bacdb
syfolen <syfolen@163.com>
2019-12-21 14:59:46 +0800

    111

--- release/sunnet.d.ts
+++ release/sunnet.d.ts
@@ -141,6 +141,30 @@ declare module sunnet {
         sendBytes(cmd:number, bytes?:Uint8Array, ip?:string, port?:number): void;
 
         /**
+         * 取消当前正在派发的事件
+         */
+        dispatchCancel(): void;
+
+        /**
+         * 事件派发
+         * @args[]: 参数列表，允许为任意类型的数据
+         * @cancelable: 事件是否允许被中断，默认为false
+         */
+        dispatchEvent(type:string, args?:any, cancelable?:boolean): void;
+
+        /**
+         * 事件注册
+         * @receiveOnce: 是否只响应一次，默认为false
+         * @priority: 事件优先级，优先级高的先被执行，默认为 1
+         */
+        addEventListener(type:string, method:Function, caller:Object, receiveOnce?:boolean, priority?:number): void;
+
+        /**
+         * 移除事件
+         */
+        removeEventListener(type:string, method:Function, caller:Object): void;
+
+        /**
          * 网络连接状态
          */
         readonly state: NetConnectionStateEnum;
@@ -156,6 +180,8 @@ declare module sunnet {
      * 自定义拦截器需要继承此类
      */
     abstract class NetConnectionInterceptor extends puremvc.Notifier implements INetConnectionInterceptor {
+
+        constructor(connection:INetConnection);
     }
 
     /**
