408bef5ad7d8b494fef5cac97e7511d399f0e834
syfolen <1041063406@qq.com>
2020-05-05 23:19:39 +0800

    111

---
+++ src/sunnet/mserver/IMSWSPacketTransferEnum.ts
@@ -0,0 +1,17 @@
+
+module sunnet {
+    /**
+     * 数据包传输方式枚举
+     */
+    export enum IMSWSPacketTransferMethodEnum {
+        /**
+         * 接收
+         */
+        RECV,
+
+        /**
+         * 发送
+         */
+        SEND
+    }
+}
---
+++ src/sunnet/protobuf/IPCMLongString.ts
@@ -0,0 +1,8 @@
+
+module sunnet {
+
+    export interface IPCMIntString {
+        arg1: number;
+        arg2: string;
+    }
+}
